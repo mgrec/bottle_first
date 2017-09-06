ae8343ce1f325b8da4a6a0161b83d76a900e24c8
Grec Maxime <maxime.grec@gmail.com>
2017-09-06 15:59:46 +0200

    add static file => CSS

--- server.py
+++ server.py
@@ -1,22 +1,30 @@
-from bottle import route, run, template, error, view
+from bottle import route, run, template, error, view, static_file
 
-#display if error 404
+
+# display if error 404
 @error(404)
 def error404(error):
     return 'Nothing here, sorry'
 
 
-#route for home
+# load css
+@route('/static/<filename>')
+def server_static(filename):
+  return static_file(filename, root='./assets/css/')
+
+
+# route for home
 @route('/')
 @view('templates/home')
 def home(name='default'):
     return dict(name=name)
 
 
-#route for home + arg name
+# route for home + arg name
 @route('/name/<name>')
 @view('templates/home')
 def name(name='default'):
     return dict(name=name)
+
 
 run(host='localhost', port=8989)
