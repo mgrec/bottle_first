92a1a60fbf41519b8ed3a38a515259f520aff4cc
Grec Maxime <maxime.grec@gmail.com>
2017-09-06 15:39:55 +0200

    add @view and args

--- server.py
+++ server.py
@@ -1,11 +1,22 @@
-from bottle import route, run, template, error
+from bottle import route, run, template, error, view
 
+#display if error 404
 @error(404)
 def error404(error):
     return 'Nothing here, sorry'
 
-@route('/')
-def home():
-    return "Hello World!"
 
-run(host='localhost', port=8080)
+#route for home
+@route('/')
+@view('templates/home')
+def home(name='default'):
+    return dict(name=name)
+
+
+#route for home + arg name
+@route('/name/<name>')
+@view('templates/home')
+def name(name='default'):
+    return dict(name=name)
+
+run(host='localhost', port=8989)
